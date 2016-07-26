b7beb6c1025a3336df58fefc61c6df39d8487898
Pete Corey <petecorey@gmail.com>
2016-07-26 14:37:51 -0400

    # Beginning Nested Expressions
    Next up on our feature todo list is adding support for nested
    expressions. A simple nested expressions to get us going could be
    `"[H]Mg"`. Let's set our expectations with a test:
    
        expect(parseMolecule("[H]Mg")).to.deep.equal({ H: 1, Mg: 1 });
    
    Now each "part" in our `parts` method could be either an element, or a
    nested expressions within square brackets. Let's update our regular
    expression to reflect that:
    
        let regex = /(\[(.*)\]|([A-Z][a-z]*))(\d*)/g;
    
    Thanks to the magic of match groups and destructing, we can assign the
    nested expression (if it exists) to `square`:
    
        let [_, __, square, part, count] = parts;
    
    Finally, if `square` exists, let's recursively process the nested
    expression and append its `parts` onto our list of `results`.
    
    With those changes, our tests flip back to green. Beautiful.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 export function molecule(formula) {
     function parts() {
         let parts, results = [];
-        let regex = /([A-Z][a-z]*)(\d*)/g;
+        let regex = /(\[(.*)\]|([A-Z][a-z]*))(\d*)/g;
         while (parts = regex.exec(formula)) {
-            let [_, part, count] = parts;
-            results.push({
-                part,
-                count: parseInt(count) || 1
-            });
+            let [_, __, square, part, count] = parts;
+            if (square) {
+                let nested = molecule(square).parts();
+                results = results.concat(nested);
+            }
+            else {
+                results.push({
+                    part,
+                    count: parseInt(count) || 1
+                });
+            }
         }
         return results;
     }
